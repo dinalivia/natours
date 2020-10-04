1f9903f99bca0269ec44c2d17d322a508c5e032b
Dina Livia <dinalivia@bct.ect.ufrn.br>
2020-10-04 16:35:45 -0400

    Initial commit
    Includes:
    - DB connection,
    - Rest API (user and tour routes),
    - CRUD operations and
    - Error handling.

---
+++ controllers/errorController.js
@@ -0,0 +1,76 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err, req, res, next) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  console.log('handleDuplicateFieldsDB()');
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  console.log(`💥 Is this error Operational? ${err.isOperational} 💥`);
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  console.log('ERROR 💥 - AT THE BEGINNG ERR IS: ', err.name);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    // let error = JSON.parse(JSON.stringify(err));
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    sendErrorProd(error, res);
+    // let error = {};
+    // if (err.name === 'CastError') {
+    //   error = handleCastErrorDB(err);
+    //   sendErrorProd(error, res);
+    // } else {
+    //   sendErrorProd(err, res);
+    // }
+  }
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+Natours Application
+Built using modern technologies: node.js, express, mongoDB and mongoose.
+Based on Jonas Schmedtmann course on Udemy - Complete node bootcamp
